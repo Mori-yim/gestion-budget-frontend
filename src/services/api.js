5ece93c23715dc91710c371d9aac51f04c8876f1
morino <morinoyimfack10@gmail.com>
2026-07-09 16:19:01 +0200

    modify package.json

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || '/api/v1'
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8083/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
